195928d2b946cab3b6acdd15eee1bae78e67745b
Thomas Paine <admin@mandatetracker.org>
2025-01-03 16:10:13 -0800

    Added packages

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Wow amazing.
+          Wow amazing!
         </a>
       </header>
     </div>
